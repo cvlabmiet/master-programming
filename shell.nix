66197d0085f26eed735407012f1894ab945549e9
Igor Sharonov <igsha@users.noreply.github.com>
2019-09-23 21:42:28 +0300

    Support CI

--- shell.nix
+++ shell.nix
@@ -4,6 +4,8 @@ mkShell rec {
   name = "master-programming";
   nativeBuildInputs = [ cmake pandoc pandoc-crossref docproc graphviz plantuml PandocElements ];
 
+  LANG = "C.UTF-8";
+
   shellHook = ''
     echo Welcome to ${name} environment!
   '';
