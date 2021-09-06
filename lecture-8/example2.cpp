babce01a59de40d0b24fb7df45ba1f7b8f0ab2b4
Igor Sharonov <igsha@users.noreply.github.com>
2021-09-06 22:45:40 +0300

    Move some lectures to asciidoc

---
+++ lecture-8/example2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+#include <boost/spirit/home/x3.hpp>
+
+int main()
+{
+    namespace x3 = boost::spirit::x3;
+
+    std::string_view text{R"(foo: bar,
+        gorp : smart ,
+        falcou : "crazy frenchman",
+        name:sam)"};
+
+    auto name = x3::rule<class name, std::string>{} // явное прописывание тэга и атрибута
+              = x3::alpha >> *x3::alnum; // переменные в C-стиле
+    auto quote = '"' >> x3::lexeme[*(~x3::char_('"'))] >> '"'; // строковые значения в кавычках
+
+    auto it = text.begin();
+    bool parsed = x3::phrase_parse(it, text.end(),
+                                   (name >> ':' >> (quote | name)) % ',',
+                                   x3::space);
+
+    std::cout << "Is parsed: " << std::boolalpha << parsed << "\n"
+              << "Position: " << std::distance(text.begin(), it) << " == " << text.size() << "\n";
+}
